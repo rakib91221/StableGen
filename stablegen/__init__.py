ad1a58cfaab22da52f4904fe862803a8c9aca3a4
sakalond <sakalaondrej@gmail.com>
2025-05-24 15:15:19 +0200

    update version id

--- stablegen/__init__.py
+++ stablegen/__init__.py
@@ -11,7 +11,7 @@ bl_info = {
     "name": "StableGen",
     "category": "Object",
     "author": "Ondrej Sakala",
-    "version": (0, 0, 5),
+    "version": (0, 0, 6),
     'blender': (4, 2, 0)
 }
 
